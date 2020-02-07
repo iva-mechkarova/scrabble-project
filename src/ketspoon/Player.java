0f196e98eef2cb23072b512ce28623564aa3e193
willisevan <evan.willis@ucdconnect.ie>
2020-02-07 19:02:02 +0000

    Added Player Class

---
+++ src/ketspoon/Player.java
@@ -0,0 +1,49 @@
+package ketspoon;
+
+
+public class Player
+{
+	private String playerName ;    
+	private int playerScore;       
+	
+	Frame playerFrame;
+	
+	
+	public Player(Frame frame, String name)
+	{
+		this.playerFrame=frame;
+		this.playerName = name;
+	}
+	
+	public void calculateScore(int score)
+	{
+		playerScore+=score;
+	}
+
+	public void resetData()  //Allows the player data to be reset//
+	{
+		playerName = null;
+		playerScore = 0;
+	}
+	
+	public void setName(String name) //Allows the name of the player to be set//
+	{
+		this.playerName = name;
+	}
+	
+	public String getName() //Allows access to a players name//
+	{
+		return playerName;
+	}
+	
+	public int getScore() //Allows access to a players score//
+	{
+		return playerScore;
+	}
+	
+	public String toString() //Allows display of a players name//
+	{
+		return "PLAYER NAME:"+playerName+"\nSCORE:"+playerScore+"\nFrame:"+playerFrame.displayFrame();
+	}
+
+}
